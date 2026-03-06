41f19d5ac3fa1e87d9c0320190eef76ff1bee178
devWillianFerreira <ferreiraw36@gmail.com>
2026-03-05 21:57:51 -0300

    feat: add asset folder and adding background in Menu

--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
 
 class Menu:
-    def __init__(self):
-        self.window = None
+    def __init__(self, window):
+        self.window = window
+        self.surf= pygame.image.load('./asset/MenuBg.png')
+        self.rect= self.surf.get_rect(left=0, top=0)
 
-    def run(self, ):
-        pass
+
+    def run(self ):
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit() # Close Window
+                    quit() #End pygame
