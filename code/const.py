e12770994011d1adc42d53a48ab946067ed36a6b
devWillianFerreira <ferreiraw36@gmail.com>
2026-03-10 21:43:01 -0300

    feat: add font to menu

--- code/const.py
+++ code/const.py
@@ -1,2 +1,17 @@
+#M
+MENU_OPTION = ("NEW GAME 1P",
+               "NEW GAME 2P - COOPERATIVE",
+               "NEW GAME 2P - COMPETITIVE",
+               "SCORE",
+               "EXIT",
+
+               )
+
+#C
+COLOR_ORANGE=(255,128,0)
+COLOR_WHITE= (255,255,255)
+
+
+#W
 WIN_WIDTH=576
 WIN_HEIGHT= 324
