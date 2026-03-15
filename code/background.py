95b4dd221bb4d394b124e18b5f3c06b10ef38a08
devWillianFerreira <ferreiraw36@gmail.com>
2026-03-15 15:47:20 -0300

    feat: Creating menu interations, Creting abstract class, creating factory and background parallax

--- code/background.py
+++ code/background.py
@@ -4,8 +4,9 @@ from code.entity import Entity
 
 
 class Background(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
+        self.rect.centerx-=1
         pass
--- code/game.py
+++ code/game.py
@@ -2,21 +2,29 @@
 # -*- coding: utf-8 -*-
 import pygame
 
-from code.const import WIN_WIDTH, WIN_HEIGHT
+from code import level
+from code.const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.level import Level
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
-        self.window = None
         pygame.init()
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
             menu= Menu(self.window)
-            menu.run()
-            pass
+            menu_return = menu.run()
+            if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
+                level_return = Level(self.window, 'Level1', menu_return)
+                level_return.run()
+            elif menu_return == MENU_OPTION[4]:
+                pygame.quit()
+                quit()
+            else:
+                pass
 
 
 
