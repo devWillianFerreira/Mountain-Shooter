6a4a8a31366d44124b7eefa96cc959ec193c9033
devWillianFerreira <ferreiraw36@gmail.com>
2026-03-17 21:20:46 -0300

    feat: add parallax background speed and level music

--- code/background.py
+++ code/background.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from code.const import WIN_WIDTH, ENTITY_SPEED
 from code.entity import Entity
 
 
@@ -7,6 +8,7 @@ class Background(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
 
-    def move(self, ):
-        self.rect.centerx-=1
-        pass
+    def move(self):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right <= 0:
+            self.rect.left = WIN_WIDTH
